5b6af145c38780ec8dc2a09729545153cfa2d40d
Mahmoud El Melligy <mahmoud-elmelligy@elarabygroup.com>
2022-06-06 09:45:20 +0200

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Mahmoud El-Melligy on 06/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
